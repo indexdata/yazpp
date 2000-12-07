5ddfd8539ebd230cd671d1b2cc639c83aa841722
Adam Dickmeiss <adam@indexdata.dk>
2000-12-07 13:27:14 +0000

    Added -I. for aclocal.

--- buildconf.sh
+++ buildconf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.3 2000-11-20 11:27:33 adam Exp $
-aclocal
+# $Id: buildconf.sh,v 1.4 2000-12-07 13:27:14 adam Exp $
+aclocal -I .
 libtoolize --force
 automake -a
 autoconf
