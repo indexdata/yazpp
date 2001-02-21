f304b5971574644559b065a70a0a964a87a55e77
Adam Dickmeiss <adam@indexdata.dk>
2001-02-21 11:25:07 +0000

    Removed aclocal.m4 and added yaz.m4.

--- buildconf.sh
+++ buildconf.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.4 2000-12-07 13:27:14 adam Exp $
-aclocal -I .
+# $Id: buildconf.sh,v 1.5 2001-02-21 11:25:07 adam Exp $
+dir=`aclocal --print-ac-dir`
+if [ -f $dir/yaz.m4 ]; then
+	aclocal
+else
+	aclocal -I .
+fi
 libtoolize --force
 automake -a
 autoconf
