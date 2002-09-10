03a58bebb2575313e5fb1709f2105541caaed237
Adam Dickmeiss <adam@indexdata.dk>
2002-09-10 12:00:20 +0000

    Make buildconf.sh verbose

--- buildconf.sh
+++ buildconf.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.8 2002-09-10 11:58:13 adam Exp $
+# $Id: buildconf.sh,v 1.9 2002-09-10 12:00:20 adam Exp $
+set -x
 dir=`aclocal --print-ac-dir`
 if [ -f $dir/yaz.m4 ]; then
 	aclocal
